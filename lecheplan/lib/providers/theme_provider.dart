95c52b94f99adb57dbe607217f0fbe27fc2689ea
IshahBautista <kofishah3@gmail.com>
2025-04-25 15:07:57 +0800

    added theme provider

---
+++ lecheplan/lib/providers/theme_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//flat colors 
+final Color darktextColor = Color(0xFF0E1342);
+final Color lighttextColor = Color(0xFFFFFFFF);
+final Color orangeAccentColor = Color(0xFFFD5903);
+final Color lightAccentColor = Color(0xFFF5EDE7);
+final Color greyAccentColor = Color(0xFFF3F1EF);
+final Color redBubbleColor = Color(0xFFFF4A4D);
+final Color unselectedGreyColor = Color(0xFF222222);
+
+//gradient colors
+final LinearGradient orangeGradient = LinearGradient(colors: [Color(0xFFFF803D), Color(0xFFFF835A)]);
+final LinearGradient whiteGradientBg = LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [Color(0xFFFFFFFF), Color(0xFFFEEFFF)]);
+
+//shadows -- please use sparingly
+final Shadow defaultShadow = Shadow(color: Colors.black12, offset: Offset(1, 2), blurRadius: 5,);
